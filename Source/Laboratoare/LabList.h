f8d7aae90a54dcd69ce0bd7b74daa77ec4250a85
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2018-10-19 01:33:57 +0300

    added Laborator 4

--- Source/Laboratoare/LabList.h
+++ Source/Laboratoare/LabList.h
@@ -3,3 +3,4 @@
 #include <Laboratoare/Laborator1/Laborator1.h>
 #include <Laboratoare/Laborator2/Laborator2.h>
 #include <Laboratoare/Laborator3/Laborator3.h>
+#include <Laboratoare/Laborator4/Laborator4.h>
